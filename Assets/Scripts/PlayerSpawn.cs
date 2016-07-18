aee2241445c7e1202c12d4824824b8a555ebcb7e
Jaswir <j.t.raghoe@students.uu.nl>
2016-07-19 00:33:50 +0200

    Main Camera  Needs PlayerController Script

---
+++ Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawn : MonoBehaviour {
+
+    public Object playerObject;
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject player = Instantiate(playerObject, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        PlayerController playerScript = player.GetComponent("PlayerController") as PlayerController;
+        playerScript.Speed = 80;
+        playerScript.RotationSpeed = 0.4f;
+        playerScript.DashStrength = 100;
+        playerScript.DashCooldown = 0.5f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
