cab32d96517039d655309ab4014c04e50516a3c8
Jordy van Opstal <jordy_van_opstal@live.nl>
2016-07-19 21:26:46 +0200

    Didn;t checkbox'd the actual ballcontroller in last commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+
+    private Vector3 _startPos;
+	// Use this for initialization
+	void Start()
+	{
+	    _startPos = transform.position;
+	}
+
+    public void ResetPosition()
+    {
+        transform.position = _startPos;
+    }
+}
