3450605729cc9a71f1e830d5b63df9a3721fc31c
Jordy van Opstal <jordy_van_opstal@live.nl>
2016-07-19 21:55:47 +0200

    Started Work on Score UI

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -7,12 +7,16 @@ public class GoalController : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Debug.Log(string.Format("Entered Goal: {0}", other.gameObject.tag));
-        if(other.gameObject.tag == "Ball")
-            GoalScored(other.gameObject);
+        if (other.gameObject.tag == "Ball")
+        {
+            BallController ball = other.GetComponent<BallController>();
+            GoalScored(ball);
+        }
     }
 
-    private void GoalScored(GameObject ball)
+    private void GoalScored(BallController ball)
     {
         Debug.Log("Goal Scored");
+        ball.ResetPosition();
     }
 }
