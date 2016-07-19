11a7f88c1dc8318d2a41fb551bc31f29dfb5d153
Jaswir <j.t.raghoe@students.uu.nl>
2016-07-19 18:51:46 +0200

    PlayerSpawn

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -1,32 +1,124 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+public class PlayerSpawn : MonoBehaviour
+{
 
-public class PlayerSpawn : MonoBehaviour {
+    public enum playingState { InActive, Active, InBetween }
+
+    public playingState p1_State = playingState.InActive;
+    public playingState p2_State = playingState.InActive;
+    public playingState p3_State = playingState.InActive;
+    public playingState p4_State = playingState.InActive;
+
+    public float p1_inactivityTimer;
+    public float p2_inactivityTimer;
+    public float p3_inactivityTimer;
+    public float p4_inactivityTimer;
 
     public Object playerObject;
-
     public int playerAmount;
+    public float maxInactivitytime;
+
+    List<GameObject> spawnedPlayers = new List<GameObject>();
     // Use this for initialization
-    void Awake()
-    {
-        for (int i = 0; i < playerAmount; i++)
-        {
-            SpawnPlayer(i+1);
-        }
-    }
 
-    // Returns amount of active controllers
-    int GetActiveControllers()
-    {
-        string[] activeControllers = Input.GetJoystickNames();
-        return activeControllers.Length;
-    }
 
-    // Spawns a Player, with appropriate playerNumber
+    // Spawns a Player, with appropriate playerNumber and adds it to the spawnedPlayers list
     void SpawnPlayer(int playerNumber)
     {
-        GameObject player = Instantiate(playerObject, new Vector3(1, 0, 1), Quaternion.identity) as GameObject;
-        PlayerController playerScript = player.GetComponent("PlayerController") as PlayerController;
-        playerScript.playerNumber = playerNumber.ToString();
+
+        bool objectExists = false;
+        for (int i = 0; i < spawnedPlayers.Count; i++)
+        {
+            PlayerController PLAYERscript = spawnedPlayers[i].GetComponent("PlayerController") as PlayerController;
+            if (int.Parse(PLAYERscript.playerNumber) == playerNumber)
+            {
+                objectExists = true;
+                break;
+            }
+        }
+
+        if (!objectExists)
+        {
+            GameObject player = Instantiate(playerObject, new Vector3(1, 0, 1), Quaternion.identity) as GameObject;
+            PlayerController playerScript = player.GetComponent("PlayerController") as PlayerController;
+            playerScript.playerNumber = playerNumber.ToString();
+            spawnedPlayers.Add(player);
+        }
+        else
+        {
+            foreach (GameObject player in spawnedPlayers)
+            {
+                PlayerController playerScript = player.GetComponent("PlayerController") as PlayerController;
+                if (playerScript.playerNumber == playerNumber.ToString())
+                {
+                    player.SetActive(true);
+                    break;
+                }
+            }
+
+        }
+    }
+   
+    //Handles spawning Finite State machine for all players
+    void FiniteStateMachine(ref playingState player_State, ref float inactivity_timer, int playerNumber)
+    {
+        //Handles InActive state => Started Pressed, goes to Active
+        if (player_State == playingState.InActive)
+        {
+            bool player_StartPressed = Input.GetButton("Start" + playerNumber);
+            if (player_StartPressed)
+            {
+                SpawnPlayer(playerNumber);
+                player_State = playingState.Active;
+            }
+        }
+
+        //Handles Active state => If x time inActivity go to InBetween
+        if (player_State == playingState.Active)
+        {
+            float player_activity = Input.GetAxis("Horizontal" + playerNumber);
+            if (player_activity != 0)
+            {
+                inactivity_timer = 0;
+            }
+            else
+            {
+                inactivity_timer += Time.deltaTime;
+            }
+
+            if (inactivity_timer >= maxInactivitytime)
+            {
+                inactivity_timer = 0;
+                player_State = playingState.InBetween;
+            }
+        }
+
+        //Handles InBetween state => Cleans up unused player
+        if (player_State == playingState.InBetween)
+        {
+            foreach (GameObject player in spawnedPlayers)
+            {
+                PlayerController playerScript = player.GetComponent("PlayerController") as PlayerController;
+                if (int.Parse(playerScript.playerNumber) == playerNumber)
+                {
+                    player.SetActive(false);
+                    player_State = playingState.InActive;
+                    return;
+                }
+            }
+        }
+
+
+    }
+
+    void Update()
+    {
+        FiniteStateMachine(ref p1_State, ref p1_inactivityTimer, 1);
+        FiniteStateMachine(ref p2_State, ref p2_inactivityTimer, 2);
+        FiniteStateMachine(ref p3_State, ref p3_inactivityTimer, 3);
+        FiniteStateMachine(ref p4_State, ref p4_inactivityTimer, 4);
     }
 }
