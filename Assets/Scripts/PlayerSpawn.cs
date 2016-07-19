58437758528af07dae8ddc62418b82a91b48225e
Jaswir <j.t.raghoe@students.uu.nl>
2016-07-19 13:32:04 +0200

    Made spawning clearer and easier, not yet workign properly

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -4,37 +4,26 @@ using System.Collections;
 public class PlayerSpawn : MonoBehaviour {
 
     public Object playerObject;
+    public int playerAmount;
     // Use this for initialization
-    void Start()
+    void Awake()
     {
-        int activeControllers = GetActiveControllers();
-        for (int i = 0; i < activeControllers; i++)
+        for (int i = 0; i < playerAmount; i++)
         {
             SpawnPlayer(i+1);
         }
     }
 
-    // Returns amount of active controllers
-    int GetActiveControllers()
-    {
-        string[] activeControllers = Input.GetJoystickNames();
-        return activeControllers.Length;
-    }
-
     // Spawns a Player, with appropriate parameters
     void SpawnPlayer(int playerNumber)
     {
-        GameObject player = Instantiate(playerObject, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        GameObject player = Instantiate(playerObject, new Vector3(1, 0, 1), Quaternion.identity) as GameObject;
         PlayerController playerScript = player.GetComponent("PlayerController") as PlayerController;
-        playerScript.Speed = 80;
-        playerScript.RotationSpeed = 0.4f;
-        playerScript.DashStrength = 100;
-        playerScript.DashCooldown = 0.5f;
         playerScript.playerNumber = playerNumber.ToString();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+
     }
 }
