0eac0d649e9aedc47b9e09aa2fc35670876d9da3
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2016-07-20 21:39:25 +0200

    Added basic audiocontroller for playing sounds

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour {
+
+    public List<AudioSetting> AudioSettings = new List<AudioSetting>();
+
+    private AudioSource _audioSource;
+
+    [Serializable]
+    public class AudioSetting {
+        public string key;
+        public AudioClip audioClip;
+    }
+
+    public void Start() {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void Play(string key) {
+        AudioClip audioClip = AudioSettings.Find(item => item.key == key).audioClip;
+        _audioSource.clip = audioClip;
+        _audioSource.Play();
+    }
+}
