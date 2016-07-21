24f3c29fbd56eea4ad6647887b4f7788e440e585
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2016-07-21 14:44:59 +0200

    Updated bomb controller with some comments

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -6,8 +6,8 @@ public class BombController : MonoBehaviour {
     public float MinDetonationTime;
     public float MaxdetonationTime;
 
-    public float MinBlinkingSpeed;
-    public float MaxBlinkingSpeed;
+    public float MaxBlinkingInterval;
+    public float MinBlinkingInterval;
     public float TimeTillMaxBlinkingSpeed;
 
     public float _detonationTime;
@@ -16,7 +16,7 @@ public class BombController : MonoBehaviour {
     {
         _detonationTime -= Time.deltaTime;
 
-        StartCoroutine("Blinking");
+        // Todo: Start Blinking!
 
         if (_detonationTime <= 0) 
         {
@@ -34,17 +34,8 @@ public class BombController : MonoBehaviour {
         // Todo: Spawn explosion
         GameManager.instance.Smoke(transform.position);
         
-        // Remove this game object
         Destroy(this.gameObject);
-    }
 
-    public IEnumerator Blinking() 
-    {
-        while (_detonationTime > 0) {
-            //Mathf.Lerp(MinBlinkingSpeed, MaxBlinkingSpeed, )
-            yield return new WaitForSeconds(1);
-        }
-        yield return null;
+        // Put back the normal ball or start new game mode, whatever
     }
-
 }
