61ce0bd3d223516f880dffd681e3261f7f629064
Jaswir <j.t.raghoe@students.uu.nl>
2016-07-21 13:32:37 +0200

    Solve conflict

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -106,12 +106,12 @@ public class GameManager : MonoBehaviour {
     /// <summary>
     /// Given a GameObject, spawns Smoke on the players position.
     /// </summary>
-    public void Smoke(GameObject obj)
+    /// <param name="position">The position to spawn the smoke</param>
+    public void Smoke(Vector3 position)
     {
         GameObject smokePrefab = (GameObject)Resources.Load("Smoke_PS");
         GameObject smoke = Instantiate(smokePrefab);
-        smoke.transform.SetParent(obj.transform);
-        smoke.transform.localPosition = new Vector3(0, 0, 0);
+        smoke.transform.position = position;
         Destroy(smoke, destroyDelay);
     }
 
