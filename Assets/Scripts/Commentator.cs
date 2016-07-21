d24323d66a576b21fd05c712e95616f7506be8a2
Jaswir <j.t.raghoe@students.uu.nl>
2016-07-21 19:24:42 +0200

    Sounds

---
+++ Assets/Scripts/Commentator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Commentator : MonoBehaviour {
+
+    private AudioController _ac;
+    public float countDownMin;
+    public float countDownMax;
+    private float countDownTimer;
+    private float countDownBoundary;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _ac = GetComponent<AudioController>();
+        countDownBoundary = Random.Range(countDownMin, countDownMax);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        countDownTimer += Time.deltaTime;
+        if(countDownTimer >= countDownBoundary)
+        {
+            int randomNum = Random.Range(0, 4);
+            _ac.Play("comment" + randomNum);
+
+            countDownTimer = 0;
+            countDownBoundary = Random.Range(countDownMin, countDownMax);
+        }
+    }
+}
